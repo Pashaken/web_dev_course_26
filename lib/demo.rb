a7ba1bb2925079897369f65aa46eda3af2490546
Pashaken <146518469+Pashaken@users.noreply.github.com>
2026-03-03 14:56:20 +0000

    Fix bags

--- lib/demo.rb
+++ lib/demo.rb
@@ -13,3 +13,7 @@ puts "Результат: #{result}"
 puts "Проверка регистра"
 puts "Я -> #{Mygem.translit('Я')}"
 puts "я. -> #{Mygem.translit('я.')}"
+
+puts "Микс из чаров"
+puts "Вот и end какbi -> #{Mygem.translit('Вот и end какbi')}"
+puts "Разные words моzhno braть -> #{Mygem.translit('Разные words моzhno braть')}"
